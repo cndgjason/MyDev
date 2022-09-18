a35e29625c0def9769504eaff3d4c8a661aaa053
jason <cndgjason@outlook.com>
2022-09-18 15:48:58 +0800

    add

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
-# flutter
+# flutter(2 steps)
 # 1.install flutter
 export PUB_HOSTED_URL=https://pub.flutter-io.cn
 export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
@@ -6,9 +6,10 @@ git clone -b stable https://gitee.com/mirrors/Flutter.git
 export PATH="$PWD/flutter/bin:$PATH"
 
 # 2.install idea
+#   install idea(dart plugin,flutter plugin), android sdk
 flutter doctor
 
-# spring
+# spring(2 steps)
 # 1.install jdk8
 
 # 2.install idea
